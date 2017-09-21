d571dab39766eba70e09e643ef4a82e90261994f
peter <pkamirimaina@gmail.com>
2017-09-21 07:51:48 +0300

    add dressing.css change index change travis

--- flaspython.py
+++ flaspython.py
@@ -5,6 +5,13 @@ app =Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+@app.route('/registration')
+def registration():
+    return render_template("registration.html")
+
+@app.route('/list')
+def list():
+    return render_template("Yourlist.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test/test_shoppinglist.py
+++
@@ -1,4 +0,0 @@
-import unittest
-
-class testshoppinglist(unittest.TestCase):
-
